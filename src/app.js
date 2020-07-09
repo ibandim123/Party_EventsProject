f9e3aef4ae0e1b2277e5e6db43d6b24b18f4ea29
Isaac Costa <ibandim@outlook.com>
2020-07-09 14:17:44 -0300

    Add chat function

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
 const methodOverride = require('method-override');
 const error = require('./middlewares/error');
+
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 
 
@@ -30,8 +35,17 @@ consign({cwd:'src'})
 //middleware de tratamento de erros
 app.use(error.notFound);
 app.use(error.serverError);
+
+io.on('connection', (client) =>{
+    client.on('send-server',(data) =>{
+        const resposta =`<b>${data.nome}:</b>${data.msg}<br>`
+        client.emit('send-client',resposta)
+        client.broadcast.emit('send-client',resposta)
+    });
+});
 //Listen Door
-app.listen(3000, ()=>{
+server.listen(3000, ()=>{
+
     console.log('App no ar!')
 })
 
